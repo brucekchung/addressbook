8f3c7f69bb92418e5ca01907fa9708bfab0c055f
Bruce Chung <brucekchung@gmail.com>
2019-05-08 22:44:39 -0600

    Can delete contacts, made style changes

--- application/routes.py
+++ application/routes.py
@@ -1,13 +1,13 @@
 from application import app, db
 from application.forms import ContactForm
 from application.models import Post
-from application.mockdata import mockdata
+# from application.mockdata import mockdata
 from flask import render_template, flash, redirect, url_for
 
 
 
 @app.route('/')
-@app.route('/home', methods=['GET', 'DELETE'])
+@app.route('/home')
 def home():
   contacts = Post.query.all()
   return render_template('home.html', contacts=contacts) 
@@ -28,3 +28,13 @@ def addcontact():
     flash(f'{contact.name.data} added to contacts!', 'success')
     return redirect(url_for('home'))
   return render_template('addcontact.html', contact=contact)
+
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
+def deletecontact(post_id):
+  contact_to_delete = Post.query.get_or_404(post_id)
+  db.session.delete(contact_to_delete)
+  db.session.commit()
+  flash(f'Goodbye {contact_to_delete.name}...', 'success')
+  return redirect(url_for('home'))
+
+

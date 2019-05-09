0a6dd5eb4469c82cdb9bb660963769f5399ed590
Bruce Chung <brucekchung@gmail.com>
2019-05-08 21:33:36 -0600

    Contacts posting to db

--- application/routes.py
+++ application/routes.py
@@ -1,22 +1,30 @@
-from application import app
+from application import app, db
 from application.forms import ContactForm
-from flask import render_template, flash, redirect, url_for
+from application.models import Post
 from application.mockdata import mockdata
+from flask import render_template, flash, redirect, url_for
 
 
-# from application.models import Post
 
 @app.route('/')
-@app.route('/home')
+@app.route('/home', methods=['GET', 'DELETE'])
 def home():
-  # actual = Post.query.all()
-  # print('postdata: ', actual)
-  return render_template('home.html', contacts=mockdata) 
+  contacts = Post.query.all()
+  return render_template('home.html', contacts=contacts) 
 
 @app.route('/addcontact', methods=['GET', 'POST'])
 def addcontact():
   contact = ContactForm()
   if contact.validate_on_submit():
+    contact_to_add = Post(
+      name = contact.name.data,
+      street = contact.street.data,
+      city = contact.city.data,
+      state = contact.state.data,
+      zipcode = contact.zipcode.data
+    ) 
+    db.session.add(contact_to_add)
+    db.session.commit()
     flash(f'{contact.name.data} added to contacts!', 'success')
     return redirect(url_for('home'))
   return render_template('addcontact.html', contact=contact)
